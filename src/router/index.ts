41ed59cc03b991d412e76aa6d2fec6d1883287f6
Mr_Spade <Mr_Spade@foxmail.com>
2023-11-09 23:27:51 +0800

    init submissions view

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ProblemsetView from '@/views/ProblemsetView.vue'
 import WorkshopView from '@/views/WorkshopView.vue'
+import SubmissionsView from '@/views/SubmissionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/workshop',
       name: 'workshop',
       component: WorkshopView
+    },
+    {
+      path: '/submissions',
+      name: 'submissions',
+      component: SubmissionsView
     },
     {
       path: '/about',
---
+++ src/views/SubmissionsView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BaseView from './BaseView.vue'
+import SubmissionsMain from '@/components/SubmissionsMain.vue'
+</script>
+
+<template>
+  <BaseView>
+    <template v-slot:breadcrumb>
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/submissions' }">
+          {{ $t('submissions') }}
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </template>
+    <SubmissionsMain />
+  </BaseView>
+</template>
