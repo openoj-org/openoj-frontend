e71744c1cf14bd0fd33b73a572fd670aa48ad841
Mr_Spade <Mr_Spade@foxmail.com>
2023-11-09 23:28:56 +0800

    delete about view

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,15 +26,15 @@ const router = createRouter({
       path: '/submissions',
       name: 'submissions',
       component: SubmissionsView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
