1c8cab452fff2070361b3785df2d66bcff2aa564
Mr_Spade <Mr_Spade@foxmail.com>
2023-11-11 16:19:03 +0800

    allow inside view

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import PageHeader from '@/components/PageHeader.vue'
 import MenuBar from '@/components/MenuBar.vue'
+const props = defineProps({
+  // true if there should be an extra blank on the top of the main page
+  inside: Boolean
+})
 </script>
 
 <template>
@@ -13,9 +17,11 @@ import MenuBar from '@/components/MenuBar.vue'
     <el-divider id="divider" />
     <el-container>
       <MenuBar />
-      <div>
-        <slot></slot>
-      </div>
+      <el-scrollbar class="main-box">
+        <div :class="{ 'main-box-inside': props.inside }">
+          <slot></slot>
+        </div>
+      </el-scrollbar>
     </el-container>
   </el-container>
 </template>
@@ -23,5 +29,14 @@ import MenuBar from '@/components/MenuBar.vue'
 <style scoped>
 #divider {
   margin: 0 auto;
+}
+.main-box {
+  width: 100%;
+  height: calc(100vh - 101.8px);
+  padding: 20px 120px;
+}
+.main-box-inside {
+  width: 100%;
+  padding: 40px 0px;
 }
 </style>
