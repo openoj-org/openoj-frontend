b83ea2226689aca9df402c8bf7ff62f095467ff4
Mr_Spade <Mr_Spade@foxmail.com>
2023-11-19 17:50:12 +0800

    separate formtable

---
+++ src/components/FormTable.vue
@@ -0,0 +1,140 @@
+<script setup lang="ts">
+import { computed, reactive, toRaw } from 'vue'
+import { ref } from 'vue'
+import {
+  ElButton,
+  ElDescriptions,
+  ElForm,
+  ElFormItem,
+  ElIcon,
+  ElInput,
+  ElSkeleton,
+  ElTable,
+  ElTableColumn
+} from 'element-plus'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const eachpageCount = 10
+
+const props = defineProps<{
+  title: string
+  loaded: boolean
+  count: number
+  searchMeta: {
+    name: string
+    icon: any
+  }[]
+  columnMeta: {
+    name: string
+    sortable: boolean
+  }[]
+  tableData: any[]
+}>()
+
+const currentPage = ref(1)
+
+const start = computed(() => {
+  return (currentPage.value - 1) * eachpageCount
+})
+const end = computed(() => {
+  return currentPage.value * eachpageCount
+})
+
+let searchData: { [index: string]: any } = {
+  order: 'id',
+  increase: true
+}
+for (let i = 0; i < props.searchMeta.length; i++) {
+  searchData[`${props.searchMeta[i].name}Keyword`] = ''
+}
+
+const tableMeta = reactive(searchData)
+
+const emit = defineEmits(['flushTableData'])
+
+const getTable = () => {
+  let Query: { [index: string]: any } = toRaw(tableMeta)
+  Query.start = start.value
+  Query.end = end.value - 1
+  console.log(Query)
+  emit('flushTableData', Query)
+}
+
+getTable()
+
+function currentChange() {
+  getTable()
+}
+
+function sortChange(arg: any) {
+  if (arg.order == 'ascending') {
+    tableMeta.order = arg.prop
+    tableMeta.increase = true
+    getTable()
+  } else if (arg.order == 'descending') {
+    tableMeta.order = arg.prop
+    tableMeta.increase = false
+    getTable()
+  }
+}
+
+function rowClick(row: any) {
+  router.push(`/${props.title}/${toRaw(row).id}`)
+}
+</script>
+
+<template>
+  <div class="box">
+    <ElDescriptions :title="$t('somethingSetting', { value: $t('keyword') })"> </ElDescriptions>
+    <ElForm :inline="true" :model="tableMeta">
+      <ElFormItem>
+        <ElInput
+          v-for="meta in searchMeta"
+          :key="meta.name"
+          v-model="tableMeta[`${meta.name}Keyword`]"
+          :placeholder="$t('keywordOfSomething', { value: $t(meta.name) })"
+        >
+          <template #prefix>
+            <ElIcon class="el-input__icon"><component :is="meta.icon" /></ElIcon>
+          </template>
+        </ElInput>
+      </ElFormItem>
+      <ElFormItem>
+        <ElButton type="primary" :disabled="!loaded" @click="getTable">{{ $t('search') }}</ElButton>
+      </ElFormItem>
+    </ElForm>
+    <el-skeleton :rows="5" animated v-if="!loaded" />
+    <div v-show="loaded">
+      <ElTable
+        :data="tableData"
+        style="width: 100%"
+        @sort-change="sortChange"
+        @row-click="rowClick"
+      >
+        <ElTableColumn
+          v-for="column in columnMeta"
+          :key="column.name"
+          :prop="column.name"
+          :label="$t(column.name)"
+          :sortable="column.sortable ? 'custom' : false"
+        />
+      </ElTable>
+      <el-pagination
+        :page-size="eachpageCount"
+        v-model:current-page="currentPage"
+        @update:current-page="currentChange"
+        layout="prev, pager, next"
+        :total="count"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  margin: 0 auto;
+  min-width: 600px;
+  max-width: 1200px;
+}
+</style>
--- src/components/main/UsersMain.vue
+++ src/components/main/UsersMain.vue
@@ -1,28 +1,22 @@
 <script setup lang="ts">
-import { computed, reactive, toRaw, type Ref } from 'vue'
+import { type Ref } from 'vue'
 import { ref } from 'vue'
-import {
-  ElButton,
-  ElDescriptions,
-  ElForm,
-  ElFormItem,
-  ElMessage,
-  ElSkeleton,
-  ElTable,
-  ElTableColumn
-} from 'element-plus'
+import { ElMessage } from 'element-plus'
 import { useRequestGet } from '@/script/service'
 import { t } from 'i18next'
 import { characterTranslate, timeIntToString } from '@/script/transform'
-import type { User as user } from '@element-plus/icons-vue'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-const eachpageCount = 10
+import { User as user } from '@element-plus/icons-vue'
+import FormTable from '../FormTable.vue'
 
 const loaded = ref(false)
-const currentPage = ref(1)
 const count = ref(0)
+
+const searchMeta = [
+  {
+    name: 'username',
+    icon: user
+  }
+]
 
 interface OriginUser {
   id: string
@@ -53,21 +47,6 @@ const columnMeta = [
 
 const tableData: Ref<User[]> = ref([])
 
-const start = computed(() => {
-  return (currentPage.value - 1) * eachpageCount
-})
-const end = computed(() => {
-  return currentPage.value * eachpageCount
-})
-
-const tableMeta = reactive({
-  order: 'id',
-  increase: true,
-  usernameKeyword: '',
-  start: start,
-  end: end
-})
-
 function userInfoTranslate(origin: OriginUser[]) {
   let result: User[] = []
   for (let i = 0; i < origin.length; i++) {
@@ -83,11 +62,11 @@ function userInfoTranslate(origin: OriginUser[]) {
   return result
 }
 
-const getTable = () => {
+const getTable = (tableMeta: { [index: string]: any }) => {
   // show loading page first
   tableData.value = []
   loaded.value = false
-  useRequestGet('/user/list', toRaw(tableMeta))
+  useRequestGet('/user/list', tableMeta)
     .then((result) => {
       if (result.data.success == false) {
         ElMessage.error(result.data.message)
@@ -102,73 +81,22 @@ const getTable = () => {
       ElMessage.error(t('unknownError'))
     })
 }
-
-getTable()
-
-function currentChange() {
-  getTable()
-}
-
-function sortChange(arg: any) {
-  if (arg.order == 'ascending') {
-    tableMeta.order = arg.prop
-    tableMeta.increase = true
-    getTable()
-  } else if (arg.order == 'descending') {
-    tableMeta.order = arg.prop
-    tableMeta.increase = false
-    getTable()
-  }
-}
-
-function rowClick(row: any) {
-  router.push(`/user/${toRaw(row).id}`)
-}
 </script>
 
 <template>
-  <div class="box">
-    <ElDescriptions :title="$t('somethingSetting', { value: $t('keyword') })"> </ElDescriptions>
-    <ElForm :inline="true" :model="tableMeta">
-      <ElFormItem>
-        <el-input
-          v-model="tableMeta.usernameKeyword"
-          :placeholder="$t('keywordOfSomething', { value: $t('username') })"
-        >
-          <template #prefix>
-            <el-icon class="el-input__icon"><user /></el-icon>
-          </template>
-        </el-input>
-      </ElFormItem>
-      <ElFormItem>
-        <ElButton type="primary" :disabled="!loaded" @click="getTable">{{ $t('search') }}</ElButton>
-      </ElFormItem>
-    </ElForm>
-    <el-skeleton :rows="5" animated v-if="!loaded" />
-    <div v-show="loaded">
-      <ElTable
-        :data="tableData"
-        style="width: 100%"
-        @sort-change="sortChange"
-        @row-click="rowClick"
-      >
-        <ElTableColumn
-          v-for="column in columnMeta"
-          :key="column.name"
-          :prop="column.name"
-          :label="$t(column.name)"
-          :sortable="column.sortable ? 'custom' : false"
-        />
-      </ElTable>
-      <el-pagination
-        :page-size="eachpageCount"
-        v-model:current-page="currentPage"
-        @update:current-page="currentChange"
-        layout="prev, pager, next"
-        :total="count"
-      />
-    </div>
-  </div>
+  <FormTable
+    title="user"
+    :loaded="loaded"
+    :count="count"
+    :search-meta="searchMeta"
+    :column-meta="columnMeta"
+    :table-data="tableData"
+    @flush-table-data="
+      (tableMeta) => {
+        getTable(tableMeta)
+      }
+    "
+  ></FormTable>
 </template>
 
 <style scoped>
