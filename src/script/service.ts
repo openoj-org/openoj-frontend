686d58d55cfd83169cc8efa8603203bc654b50ba
Mr_Spade <Mr_Spade@foxmail.com>
2023-11-20 16:45:31 +0800

    complete request download

--- src/script/service.ts
+++ src/script/service.ts
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import global from '@/assets/global.json'
+import FileSaver from 'file-saver'
+import { ElMessage } from 'element-plus'
+import { t } from 'i18next'
 
 const request = axios.create({
   baseURL: global.backendUrl,
-  timeout: 1000
+  timeout: 10000
 })
 
 export function useRequestGet(url: string, query: object) {
@@ -12,4 +15,19 @@ export function useRequestGet(url: string, query: object) {
 
 export function useRequestPost(url: string, query: object) {
   return request.post(url, query)
+}
+
+export function useRequestDownload(url: string, query: object, filename: string) {
+  request
+    .get(url, {
+      params: query,
+      responseType: 'blob'
+    })
+    .then((result) => {
+      FileSaver.saveAs(result.data, filename)
+    })
+    .catch((err) => {
+      console.log(err)
+      ElMessage.error(t('unknownError'))
+    })
 }
