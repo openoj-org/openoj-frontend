727c672d54c6705e45b46cd4ebab3b4f59f8386b
Mr_Spade <Mr_Spade@foxmail.com>
2023-11-28 15:55:20 +0800

    split convert function && create WorkInfo type

--- src/script/types.ts
+++ src/script/types.ts
@@ -1,4 +1,4 @@
-interface LinkBody {
+export interface LinkBody {
   head: string
   idName: string
 }
@@ -144,6 +144,15 @@ export interface Sample {
   output?: string
 }
 
+function convertTimeLimit(data: number) {
+  if (data >= 1000) return `${data / 1000}s`
+  return `${data}ms`
+}
+function convertMemoryLimit(data: number) {
+  if (data >= 1024) return `${data / 1024}GB`
+  return `${data}MB`
+}
+
 export class ProblemInfo extends ProblemBaseInfo {
   titleEn: string
   type: 0
@@ -155,21 +164,38 @@ export class ProblemInfo extends ProblemBaseInfo {
   outputStatement: string
   rangeAndHint: string
   samples: Sample[]
-  static convertTimeLimit(data: number) {
-    if (data >= 1000) return `${data / 1000}s`
-    return `${data}ms`
-  }
-  static convertMemoryLimit(data: number) {
-    if (data >= 1024) return `${data / 1024}GB`
-    return `${data}MB`
-  }
   constructor(data: any) {
     super(data)
     this.titleEn = data.titleEn == undefined ? '' : data.titleEn
     this.type = data.type == undefined ? 0 : data.type
-    this.timeLimit = data.timeLimit == undefined ? '' : ProblemInfo.convertTimeLimit(data.timeLimit)
-    this.memoryLimit =
-      data.memoryLimit == undefined ? '' : ProblemInfo.convertMemoryLimit(data.memoryLimit)
+    this.timeLimit = data.timeLimit == undefined ? '' : convertTimeLimit(data.timeLimit)
+    this.memoryLimit = data.memoryLimit == undefined ? '' : convertMemoryLimit(data.memoryLimit)
+    if (data.background != undefined) this.background = data.background
+    this.statement = data.statement == undefined ? '' : data.statement
+    this.inputStatement = data.inputStatement == undefined ? '' : data.inputStatement
+    this.outputStatement = data.outputStatement == undefined ? '' : data.outputStatement
+    this.rangeAndHint = data.rangeAndHint == undefined ? '' : data.rangeAndHint
+    this.samples = data.samples == undefined ? [] : data.samples
+  }
+}
+
+export class WorkInfo extends WorkBaseInfo {
+  titleEn: string
+  type: 0
+  timeLimit: string
+  memoryLimit: string
+  background?: string
+  statement: string
+  inputStatement: string
+  outputStatement: string
+  rangeAndHint: string
+  samples: Sample[]
+  constructor(data: any) {
+    super(data)
+    this.titleEn = data.titleEn == undefined ? '' : data.titleEn
+    this.type = data.type == undefined ? 0 : data.type
+    this.timeLimit = data.timeLimit == undefined ? '' : convertTimeLimit(data.timeLimit)
+    this.memoryLimit = data.memoryLimit == undefined ? '' : convertMemoryLimit(data.memoryLimit)
     if (data.background != undefined) this.background = data.background
     this.statement = data.statement == undefined ? '' : data.statement
     this.inputStatement = data.inputStatement == undefined ? '' : data.inputStatement
